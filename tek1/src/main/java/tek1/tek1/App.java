9386012d9800ed2641e1d80f28d82c32f6d38dcc
gerasim81 <gerasim81@rambler.ru>
2016-10-14 12:58:47 +0300

    2 master commit

--- tek1/src/main/java/tek1/tek1/App.java
+++ tek1/src/main/java/tek1/tek1/App.java
@@ -1,12 +1,63 @@
 package tek1.tek1;
 
+import java.util.Random;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	//main
-    	//main1
-    	//main2
-    	//main3
-    }
+	public static void main( String[] args )
+	{
+
+		Bubble bubble = new Bubble();
+		Brush brush = new Brush();
+		Random random = new Random();
+
+		int razmer = random.nextInt(999);
+
+		int [] array1 = new int[razmer];//выделяем память для массива
+		int [] array2 = new int[razmer];//выделяем память для массива
+
+
+		for (int i =0; i < array1.length;i++){
+			//array1[i]= random.nextInt();
+			//array1[i]= random.nextInt(99);
+			array1[i]= random.nextInt(999);
+		}
+		for (int i =0; i < array2.length;i++){
+			//array2[i]= random.nextInt();
+			//array2[i]= random.nextInt(99);
+			array2[i]= random.nextInt(999);
+		}
+
+		random = null;//
+				
+		System.out.println("Исходный массив:");
+		for (int dig1 : array1){
+			System.out.print(dig1 + " ");
+		}
+		System.out.println();
+
+		array1 = bubble.sortBubble(array1);
+
+		System.out.println("Отсортированный пузырьком массив:");
+		for (int dig1 : array1){
+			System.out.print(dig1 + " ");
+		}
+		System.out.println();
+		
+		//--------------------------------------------
+
+		System.out.println("Исходный массив:");
+		for (int dig1 : array2){
+			System.out.print(dig1 + " ");
+		}
+		System.out.println();
+		
+		array2 = brush.sortBrush(array2);
+
+		System.out.println("Отсортированный расческой массив:");
+		for (int dig1 : array2){
+			System.out.print(dig1 + " ");
+		}
+		System.out.println();
+	}
 }
